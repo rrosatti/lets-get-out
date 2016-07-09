6ba3d4f330812c430594f63d8af74bb8dbc4dc40
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-07-09 08:58:57 -0300

    override methods onCreateOptionsMenu() and onOptionsItemSelected()

--- app/src/main/java/com/example/rodri/portugalproject/MainActivity.java
+++ app/src/main/java/com/example/rodri/portugalproject/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -117,6 +118,28 @@ public class MainActivity extends AppCompatActivity {
         drawerItems.add(new DrawerItem(menuTitles[1], menuIcons.getResourceId(1, -1)));
 
         menuIcons.recycle();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     @Override
