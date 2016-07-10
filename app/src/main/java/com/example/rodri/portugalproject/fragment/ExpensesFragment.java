d94aeb4ab075e165eb14676f3cd62f1213d480ae
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-07-10 13:21:39 -0300

    create ExpensesFragment.java (not fully implemented)

---
+++ app/src/main/java/com/example/rodri/portugalproject/fragment/ExpensesFragment.java
@@ -0,0 +1,42 @@
+package com.example.rodri.portugalproject.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rodri.portugalproject.R;
+
+/**
+ * Created by rodri on 7/10/2016.
+ */
+public class ExpensesFragment extends Fragment {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    public ExpensesFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_expenses, null);
+
+        toolbar = (Toolbar) v.findViewById(R.id.toolbarExpenses);
+        viewPager = (ViewPager) v.findViewById(R.id.viewPagerExpenses);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) v.findViewById(R.id.tabLayoutExpenses);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return v;
+    }
+
+}
