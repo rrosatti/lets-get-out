d8330526caf6076f171759f5093eb53abdd863e3
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-07-16 10:10:29 -0300

    change layout from activity_setup_goal.xml(add toolbar). Make toolbar back button works in SetUpGoalActivity

--- app/src/main/java/com/example/rodri/letsgetout/activity/SetUpGoalActivity.java
+++ app/src/main/java/com/example/rodri/letsgetout/activity/SetUpGoalActivity.java
@@ -3,18 +3,36 @@ package com.example.rodri.letsgetout.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.example.rodri.letsgetout.R;
+import com.example.rodri.letsgetout.util.Util;
 
 /**
  * Created by rodri on 7/16/2016.
  */
 public class SetUpGoalActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Util.setFullScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_goal);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbarSetUpGoal);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
