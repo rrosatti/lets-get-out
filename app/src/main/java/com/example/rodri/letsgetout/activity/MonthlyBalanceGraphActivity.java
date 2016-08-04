bedd3c1efe3c00d97c40c4e1356a596d192e98f8
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-08-04 14:18:47 -0300

    create activity_monthly_balance_graph.xml and MonthlyBalanceGraphActivity.java

---
+++ app/src/main/java/com/example/rodri/letsgetout/activity/MonthlyBalanceGraphActivity.java
@@ -0,0 +1,19 @@
+package com.example.rodri.letsgetout.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rodri.letsgetout.util.Util;
+
+/**
+ * Created by rodri on 8/4/2016.
+ */
+public class MonthlyBalanceGraphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Util.setFullScreen(this);
+        super.onCreate(savedInstanceState);
+
+    }
+}
